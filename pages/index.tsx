dae446e2b21a514d1a74322f8011244f3a339034
Limttugi <junhuk820@gmail.com>
2023-01-14 02:41:52 +0900

    feat: 가맹점 조회 섹션 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import DisplayPayment from '@/components/Section/DisplayPayment';
 import MainDescription from '@/components/Section/MainDescription';
+import StoreInquiry from '@/components/Section/StoreInquiry';
 
 const Index = () => {
   return (
     <main>
       <MainDescription />
       <DisplayPayment />
+      <StoreInquiry />
     </main>
   );
 };
