e42d930c567f06c59c8bb032241e5e5b7426a810
Limttugi <junhuk820@gmail.com>
2023-02-15 19:50:45 +0900

    fix: 잘못된 코드 수정

--- utils/setViewType.ts
+++ utils/setViewType.ts
@@ -1,6 +1,6 @@
 const setViewType = (width: number) => {
   if (width < 768) return 'MOBILE';
-  else if (width >= 768 && width < 1920) return 'TABLET';
+  else if (width >= 768 && width < 1024) return 'TABLET';
   else return 'DESKTOP';
 };
 
