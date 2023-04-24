64667c6be282da55efa90b8846d472b4626eb4ed
Limttugi <junhuk820@gmail.com>
2023-04-24 22:19:39 +0900

    refactor: 헤더 웹 접근성 향상 (h2 -> h1, 마크업 구조 변경)

--- components/_Atoms/Link/MainLogo_Link/MainLogo_Link.tsx
+++ components/_Atoms/Link/MainLogo_Link/MainLogo_Link.tsx
@@ -7,10 +7,11 @@ import MainLogo from '@/img/Logo/TapPlace/tapplace_logo.webp';
 
 const MainLogo_Link = () => {
   return (
-    <Link href='/' className={s.linkContainer}>
-      <h2 className='a11y-hidden'>DefaultPageLink</h2>
-      <Image src={MainLogo} alt='TapplaceLogoImage' />
-    </Link>
+    <h1 className={s.linkContainer} data-service-name='TapPlace'>
+      <Link href='/'>
+        <Image src={MainLogo} alt='탭플레이스' />
+      </Link>
+    </h1>
   );
 };
 
